6aac8be76f79fadb7e50f83e3df9f9e62e564a09
Andreas Waidler <waidler@programmers.at>
2021-01-16 15:40:16 +0100

    Import skeleton generated by start.spring.io

---
+++ src/main/java/io/github/consolero/bootbootboot/BootBootingBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.consolero.bootbootboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBootingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBootingBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/consolero/bootbootboot/BootBootingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.consolero.bootbootboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBootingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
